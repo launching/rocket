9b95d4a3dc510822ae958fd3569c0f7d8386a939
lidenghui <3379489032@qq.com>
2019-03-10 23:07:41 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<style>
+html {
+    position: relative;
+    min-height: 100%;
+    background: #ffffff;
+}
+</style>
+<template>
+    <ro-layout></ro-layout>
+</template>
+<script>
+import RoLayout from "./views/Layout.vue";
+export default {
+    components: {
+        RoLayout
+    },
+    data() {
+        let nums = [];
+        let i = 100;
+        do {
+            nums.push(i);
+            i--;
+        } while (i >= 0);
+        return {
+            nums
+        };
+    }
+};
+</script>
---
+++ src/views/Layout.vue
@@ -0,0 +1,95 @@
+<style lang="scss" >
+.v-layout {
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: absolute;
+    border-radius: 4px;
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    .layout-logo {
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav {
+        width: 420px;
+        margin: 0 auto;
+        margin-right: 20px;
+    }
+}
+</style>
+<template>
+    <Layout class="v-layout">
+        <Header>
+            <Menu mode="horizontal" theme="dark" active-name="1">
+                <div class="layout-logo"></div>
+                <div class="layout-nav">
+                    <MenuItem name="1">
+                        <Icon type="ios-navigate"></Icon>Item 1
+                    </MenuItem>
+                    <MenuItem name="2">
+                        <Icon type="ios-keypad"></Icon>Item 2
+                    </MenuItem>
+                    <MenuItem name="3">
+                        <Icon type="ios-analytics"></Icon>Item 3
+                    </MenuItem>
+                    <MenuItem name="4">
+                        <Icon type="ios-paper"></Icon>Item 4
+                    </MenuItem>
+                </div>
+            </Menu>
+        </Header>
+        <Layout>
+            <Sider hide-trigger :style="{ background: '#fff' }">
+                <Menu
+                    active-name="1-1"
+                    theme="light"
+                    width="auto"
+                    :open-names="['1']"
+                >
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate"></Icon>帐号管理
+                        </template>
+                        <MenuItem name="1-1">用户管理</MenuItem>
+                        <MenuItem name="1-2">角色管理</MenuItem>
+                        <MenuItem name="1-3">权限管理</MenuItem>
+                    </Submenu>
+                    <Submenu name="2">
+                        <template slot="title">
+                            <Icon type="ios-keypad"></Icon>设置管理
+                        </template>
+                        <MenuItem name="2-1">页面设置</MenuItem>
+                        <MenuItem name="2-2">字典管理</MenuItem>
+                    </Submenu>
+                </Menu>
+            </Sider>
+            <Layout
+                :style="{
+                    padding: '0 24px 24px'
+                }"
+            >
+                <router-view></router-view>
+            </Layout>
+        </Layout>
+    </Layout>
+</template>
+<script>
+export default {
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+    mounted() {}
+};
+</script>
